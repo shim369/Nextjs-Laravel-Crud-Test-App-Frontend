0d8d72d5b1dc6aa579cd0f8182bbc6ae4b5b3ead
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-09 18:16:35 +0900

    add add task component

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,64 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+import styles from "../page.module.css";
+import axios from 'axios';
+
+interface Task {
+    name: string;
+    url: string;
+}
+
+export default function AddTask() {
+    const [task, setTask] = useState<Task>({ name: '', url: '' });
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        let errors = [];
+        if (!task.name) {
+            errors.push("Task name is required!")
+        }
+        if (!task.url) {
+            errors.push("Task url is required!")
+        }
+        if (!errors.length) {
+            let formData = new FormData();
+            formData.append('name', task.name);
+            formData.append('url', task.url);
+            let url = 'http://127.0.0.1:8000/api/save_task';
+            await axios.post(url, formData).then((response) => {
+                console.log(response);
+                if (response.status == 200) {
+                    setTask({ name: '', url: '' });
+                    alert(response.data.message);
+                } else {
+                    console.log('error');
+                }
+            }).catch(error => {
+                errors.push(error.response);
+            });
+        }
+    }
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setTask(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    return (
+        <form className={styles.taskForm} onSubmit={handleSubmit}>
+            <div className={styles.addBox}>
+                <input type="text" name="name" className={styles.taskInput} onChange={handleInputChange} placeholder="Add Task Name" autoComplete="off" />
+                <input type="text" name="url" className={styles.taskInput} onChange={handleInputChange} placeholder="Add Task Url" autoComplete="off" />
+                <div className={styles.formAlert}>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+                <button type="submit" className={styles.formSubmit}>submit</button>
+            </div>
+        </form>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import styles from "./page.module.css";
 import TaskList from "./components/TaskList";
+import AddTask from "./components/AddTask";
 
 export default function Home() {
   return (
     <main>
-      <h1>Next.js Laravel Todo App</h1>
+      <h1 className={styles.title}>Next.js Laravel Todo App</h1>
       <section className={styles.tasksContainer}>
+        <AddTask />
         <TaskList />
       </section>
     </main>
