f46afbe9bdbb3a3edab33ceb11f4622ff4c05ffe
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-24 09:22:14 +0900

    update Home

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,70 @@
 import styles from "./page.module.css";
 import TaskList from "./components/TaskList";
 import AddTask from "./components/AddTask";
-import { useEffect, useState } from "react";
+import React,{ useEffect, useState } from "react";
 import { Todo } from "@/types/todo";
 import axios from "axios";
 
 export default function Home() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [task, setTask] = useState<Todo>({ id: 0, name: '', url: '', completed: false });
+  const [tasks, setTasks] = useState<Todo[]>([]);
+  const [errors, setErrors] = useState<string[]>([]);
+
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setTask(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    const data = {
+      name: task.name,
+      url: task.url,
+    }
+  
+    let errors: string[] = [];
+  
+    if (!task.name) {
+      errors.push("Task name is required!")
+    }
+    if (!task.url) {
+      errors.push("Task url is required!")
+    }
+    
+    setErrors(errors);
+  
+    if (!errors.length) {
+      try {
+        let url = 'http://127.0.0.1:8000/api/save_task';
+        const response = await axios.post(url, data);
+        if (response.status == 200) {
+          setTask({ id: 0, name: '', url: '', completed: false });
+          alert(response.data.message);
+        }
+      } catch (error: any) {
+        errors.push(error.response);
+      }
+    }
+  }
 
   useEffect(() => {
     const getAllTasks = async () => {
       try {
         let url = 'http://127.0.0.1:8000/api/tasks';
         const response = await axios.get(url);
-        setTodos(response.data.tasks);
+        setTasks(response.data.tasks);
       } catch (error) {
         console.log(error);
       }
     };
 
     getAllTasks();
-  }, [todos]);
+  }, [tasks]);
 
   const deleteTask = async (id: Number) => {
     try {
@@ -29,7 +73,7 @@ export default function Home() {
       const response = await axios.delete(url);
       if (response.data.code == 200) {
         alert(response.data.message);
-        setTodos(todos.filter((todo) => todo.id !== id));
+        setTasks(tasks.filter((task) => task.id !== id));
       }
     } catch (error) {
       console.log(error);
@@ -42,13 +86,17 @@ export default function Home() {
       <h1 className={styles.title}>Next.js Laravel Todo App</h1>
       <section className={styles.tasksContainer}>
         <AddTask
+          task={task}
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
+          errors={errors}
         />
         <div className={styles.tasks}>
-          {todos.map((todo) => {
+          {tasks.map((task) => {
             return (
               <TaskList
-                key={todo.id}
-                todo={todo}
+                key={task.id}
+                task={task}
                 onDeleteClick={deleteTask}
               />
             );
